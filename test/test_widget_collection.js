1c0059db22aa1e3b411d2ff8a334e96e465b7bba
Etienne Rossignon <etienne.rossignon@gadz.org>
2017-06-15 21:57:54 +0200

    Add ability to extract a subset of a collection.

--- test/test_widget_collection.js
+++ test/test_widget_collection.js
@@ -13,13 +13,13 @@ class MyWidgetConnector extends WidgetConnector
 
 class MyWidget extends WidgetBase
 {
-    constructor() {
-        super();
+    constructor(name) {
+        super(name);
         this.myLink1 = new MyWidgetConnector(this);
     }
 
     clone() {
-        const clone = new MyWidget();
+        const clone = new MyWidget(this.name);
         clone.myLink1.set(this.myLink1.get());
         return clone;
     }
@@ -40,9 +40,9 @@ describe("WidgetCollection",function(){
 
         const c = new MyWidgetCollection();
 
-        const w1 = c.addSomeWidget();
-        const w2 = c.addSomeWidget();
-        const w3 = c.addSomeWidget();
+        const w1 = c.addSomeWidget("w1");
+        const w2 = c.addSomeWidget("w2");
+        const w3 = c.addSomeWidget("w3");
         w1._id.should.equal(1);
         w2._id.should.equal(2);
         w3._id.should.equal(3);
@@ -51,9 +51,9 @@ describe("WidgetCollection",function(){
     it("should provide a list of possible widget to connect to",function() {
 
         const c = new MyWidgetCollection();
-        const w1 = c.addSomeWidget();
-        const w2 = c.addSomeWidget();
-        const w3 = c.addSomeWidget();
+        const w1 = c.addSomeWidget("w1");
+        const w2 = c.addSomeWidget("w2");
+        const w3 = c.addSomeWidget("w3");
 
         c.getPossibleAncestors(w1).length.should.eql(0);
         c.getPossibleAncestors(w2).length.should.eql(1);
@@ -68,9 +68,9 @@ describe("WidgetCollection",function(){
 
         const c = new MyWidgetCollection();
 
-        const w1 = c.addSomeWidget();
-        const w2 = c.addSomeWidget();
-        const w3 = c.addSomeWidget();
+        const w1 = c.addSomeWidget("w1");
+        const w2 = c.addSomeWidget("w2");
+        const w3 = c.addSomeWidget("w3");
 
         w1.getDependantEntities().length.should.eql(0);
         w2.getDependantEntities().length.should.eql(0);
@@ -105,9 +105,9 @@ describe("WidgetCollection",function(){
 
     it("should be able to edit one element and replace it",function() {
         const c = new MyWidgetCollection();
-        const w1 = c.addSomeWidget();
-        const w2 = c.addSomeWidget();
-        const w3 = c.addSomeWidget();
+        const w1 = c.addSomeWidget("w1");
+        const w2 = c.addSomeWidget("w2");
+        const w3 = c.addSomeWidget("w3");
 
 
         w2.myLink1.set(w1);
@@ -129,9 +129,50 @@ describe("WidgetCollection",function(){
         const w2 = c.addSomeWidget();
         w2.myLink1.set(w1);
 
-        w2.canDelete().should.eql(true);
+        c.items.length.should.eql(2);
 
-       - c.deleteItem(w2);
-    })
+
+        w2.canDelete().should.eql(true);
+        c.deleteItem(w2);
+
+        c.items.length.should.eql(1);
+    });
+
+
+    it("should be possible to extract a sub set ",function() {
+
+        const c = new MyWidgetCollection();
+        const w1 = c.addSomeWidget("w1");
+        const w2 = c.addSomeWidget("w2");
+        w2.myLink1.set(w1);
+
+        const w3 = c.addSomeWidget("w3");
+        w3.myLink1.set(w1);
+
+        const w4 = c.addSomeWidget("w4");
+
+        w1.getDependantEntities().length.should.eql(2,"w1 is referenced by w2 and w3");
+        w2.getDependantEntities().length.should.eql(0);
+        w4.getDependantEntities().length.should.eql(0);
+
+        const subset = c.extractSubset([w2,w4]);
+         subset.should.be.instanceof(MyWidgetCollection);
+
+        subset.items.length.should.eql(3);
+
+        subset.items.map(x=>x._id).join(" ").should.eql("1 2 4");
+
+        subset.items[0]._id.should.eql(w1._id);
+        subset.items[1]._id.should.eql(w2._id);
+        subset.items[2]._id.should.eql(w4._id);
+
+        const ww1 = subset.items[0];
+        const ww2 = subset.items[1];
+        const ww4 = subset.items[2];
+
+        ww1.getDependantEntities().length.should.eql(1);
+        ww2.getDependantEntities().length.should.eql(0);
+        ww4.getDependantEntities().length.should.eql(0);
+    });
 
 });
